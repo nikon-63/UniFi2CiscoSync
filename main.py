b329698365ff2c0a98e2b3dfe5a50ca11bd78395
Tom N <79413139+nikon-63@users.noreply.github.com>
2025-06-01 21:30:07 +0100

    Added compre function

--- main.py
+++ main.py
@@ -36,11 +36,41 @@ def sorted_networks(json_data):
         sys.exit(f"ERROR: No VLAN-enabled networks found.")
     return result
 
+def compare_networks(unifi_vlans, cisco_vlans):
+    unifi_vlan_ids = set(str(vlan["vlan"]) for vlan in unifi_vlans)
+    cisco_vlan_ids = set(str(vlan["vlan"]) for vlan in cisco_vlans)
+    # Checking to see if there are any vlans on the UniFi that are not on the Cisco switch
+    for vlan in unifi_vlans:
+        vlan_id = str(vlan["vlan"])
+        vlan_name = vlan["name"]
+        if vlan_id not in cisco_vlan_ids:
+            print(f"Adding VLAN {vlan_id} ({vlan_name}) to Cisco switch...")
+            cisco_make_network(vlan_id, vlan_name)
+    # Checking to see if there are any vlans on the Cisco switch that are not on the UniFi
+    for vlan in cisco_vlans:
+        vlan_id = str(vlan["vlan"])
+        if vlan_id not in unifi_vlan_ids:
+            print(f"Deleting VLAN {vlan_id} ({vlan['name']}) from Cisco switch...")
+            cisco_delete_network(vlan_id)
 
 def main():
     print("Script started.")
     unifi_networks = unifi_connect()
-    vlan_networks = sorted_networks(unifi_networks)
+    vlan_networks_unifi = sorted_networks(unifi_networks)
+    vlan_networks_cisco = cisco_fetch_networkconf()
+
+    print("Printing vlan_networks_unifi:")
+    print(vlan_networks_unifi)
+    print("Printing vlan_networks_cisco:")
+    print(vlan_networks_cisco)
+
+    # TODO: Put this into a function with error handling
+    cisco_vlan_list = [
+        {"vlan": vlan["vlan"], "name": vlan["name"]}
+        for vlan in vlan_networks_cisco.get("data", [])
+    ]
+
+    compare_networks(vlan_networks_unifi, cisco_vlan_list)
 
 
 if __name__ == "__main__":
